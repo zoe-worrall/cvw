6565fb0aca6f3fece548324e5c0d7e2ee5e687c0
Zoe Worrall <zoeworrall2021@gmail.com>
2025-02-10 14:48:38 -0800

    checking multiplication alas

--- examples/exercises/fma16/fma16_fmul.sv
+++ examples/exercises/fma16/fma16_fmul.sv
@@ -32,11 +32,11 @@ assign sign_z = ((sign_x & sign_y) | (~sign_x & ~sign_y)) ? 0 : 1; // aka XOR
 assign exp_x = x[14:11];
 assign exp_y = y[14:11];
 
-assign frac_z = x[11:0]*y[11:0];
+assign frac_z = x[11]*y[11];
 
 assign exp_z = (frac_z[11]) ? exp_x + exp_y + 1 : exp_x + exp_y;
 
-assign result = {4'b0, frac_z}; //{sign_z, exp_z, frac_z};
+assign result = {x[11], y[11], 2'b0, frac_z}; //{sign_z, exp_z, frac_z};
 assign flags = 0;
 
 endmodule
