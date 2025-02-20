f45fcda1328a12d8a1c16d18fbe5a1b79053d578
zoe-worrall <zoeworrall2021@gmail.com>
2025-02-20 03:24:27 -0800

    Confirmed fmul_2 works
    Confirmed using test bench that fmul_2 works properly

--- examples/exercises/fma16/testbench.sv
+++ examples/exercises/fma16/testbench.sv
@@ -21,7 +21,7 @@ module testbench_fma16;
   // at start of test, load vectors and pulse reset
   initial
     begin
-      $readmemh("C:/Users/zoeworrall/Desktop/projects_LRS/project_testing/fma_special_rne.tv", testvectors);
+      $readmemh("C:/Users/zoeworrall/Desktop/projects_LRS/project_testing/fmul_2.tv", testvectors);
       vectornum = 0; errors = 0;
       reset = 1; #22; reset = 0;
     end
