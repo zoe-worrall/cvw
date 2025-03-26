479ac4f712068bbe69dc26841cdba737b83d6056
Zoe Worrall <zoeworrall2021@gmail.com>
2025-03-26 08:46:39 -0700

    Fixed Lint Issues, checking now

--- examples/exercises/fma16/fma16.sv
+++ examples/exercises/fma16/fma16.sv
@@ -34,10 +34,10 @@ output [3:0]  flags;
 
 logic [9:0]   xm, ym, zm;
 logic [21:0]  mid_pm;
-logic [146:0] pm;
+logic [136:0] pm;
 logic [4:0]   xe, ye, ze;
 logic [5:0]   pe;
-logic [1]     xs, ys, zs, ms;
+logic         xs, ys, zs, ms;
 
 parameter inf_val = 16'b0_11111_0000000000;
 parameter nan_val = 16'b0_11111_0000000001;
@@ -63,57 +63,59 @@ assign pe = xe + ye - 5'b01111;  // -15 for normalization
 
 // Step #3 - Alignment Shift Count
 logic [5:0] a_cnt;
-assign a_cnt = pe - ze - 6'b001111;   // maximum is 32
+logic       a_cnt_positive;
+assign a_cnt_positive = (pe > {1'b0, ze});
+assign a_cnt = a_cnt_positive ? pe - ze - 6'b001111 : ze - pe - 6'b001111 ;   // maximum is 32
 
 // Step #4 - Alignment Mantissa
-logic [146:0] zm_bf_shift;
-logic [146:0] am;
+logic [136:0] zm_bf_shift;
+logic [136:0] am;
 assign zm_bf_shift = { 63'h0, 1'b1, zm, 63'h0 };
 assign am = zm_bf_shift >> a_cnt;    // left shift
 
 // Step #5 - Sum Mantissa
-logic [146:0] sm;
+logic [136:0] sm;
 assign sm = z_zero ? pm : (am + pm);
 
 // Step #6 - Normalization Shift
 logic [7:0] m_cnt;
 always_comb begin // logic based off FMA Detailed Algorithm
-    // if a_cnt is between  0 and  21
-    if ((a_cnt >= 6'd0) & (a_cnt <= 6'd20)) begin
+    m_cnt = 0;
+    if (a_cnt_positive & (a_cnt < 6'd21)) begin
         if       (sm[76]) m_cnt = -2;
         else if  (sm[75]) m_cnt = -1;
         else              m_cnt =  0;
     end
-    else if ((a_cnt >= -4) & (a_cnt <= -1)) begin
-        if       (sm[75 - a_cnt]) m_cnt = a_cnt - 1;
-        else                      m_cnt = a_cnt;
+    else if (~a_cnt_positive & (a_cnt > 1) & (a_cnt < 4)) begin
+        if       (sm[75 - a_cnt]) m_cnt = { 2'b00, a_cnt} - 8'b1;
+        else                      m_cnt = { 2'b00, a_cnt};
     end
-    else if (a_cnt < -4) begin
-        if       (sm[75]) m_cnt = a_cnt;
+    else if (~a_cnt_positive & (a_cnt > 4)) begin
+        if       (sm[75]) m_cnt = { 2'b00, a_cnt};
     end
-    else if (a_cnt > 21) begin
+    else if (a_cnt_positive & (a_cnt > 21)) begin
         if       (sm[76]) m_cnt = -1;
         else              m_cnt =  0;
     end
 end
 
 // Step #7 - Normalization Mantissa and Exponent
-logic [146:0] mm;
+logic [136:0] mm;
 logic [9:0] mm_part;
-logic [4:0] me;
+logic [7:0] me;
 logic [7:0] index;
 
-assign mm = (m_cnt > 1) ? sm << (m_cnt - product_carried) : sm >> (product_carried - m_cnt);
+assign mm = (m_cnt > 1) ? sm << (m_cnt - {7'b0, product_carried}) : sm >> ({7'b0, product_carried} - m_cnt);
 assign mm_part = mm[72:63];
-assign me = product_carried ? (pe - m_cnt + 1) : (pe - m_cnt);
+assign me = product_carried ? ({2'b0, pe} - m_cnt + 8'b1) : ({2'b00, pe} - m_cnt);
 
 
 // Not a Step - Assign First Bit
-assign ms = (pe > ze) ? ((~xs & ys) | (xs & ~ys)) : zs;//~(xs ^ ys) : zs;
+assign ms = (pe > {1'b0, ze}) ? ((~xs & ys) | (xs & ~ys)) : zs;//~(xs ^ ys) : zs;
 
 // Combine together (no rounding yet)
 // I'll need to account for the possibility that z is negative in the future, since that'll change the sign bit
-assign result =  {ms, me, mm_part};
+assign result =  {ms, me[4:0], mm_part};
 assign flags = 4'b0;
 
 
