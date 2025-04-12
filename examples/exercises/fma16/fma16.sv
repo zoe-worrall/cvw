6ee9cd7d27287422048f8f92561d762656435540
Zoe Worrall <zoeworrall2021@gmail.com>
2025-04-12 12:18:53 -0700

    lint clean version

--- examples/exercises/fma16/fma16.sv
+++ examples/exercises/fma16/fma16.sv
@@ -34,10 +34,10 @@ output [3:0]  flags;
 
 logic [9:0]   xm, ym, zm;
 logic [21:0]  mid_pm;
-logic [146:0] pm;
+logic [136:0] pm;
 logic [4:0]   xe, ye, ze;
 logic [5:0]   pe;
-logic [1]     xs, ys, zs, ms;
+logic         xs, ys, zs, ms;
 
 parameter inf_val = 16'b0_11111_0000000000;
 parameter nan_val = 16'b0_11111_1000000000;
@@ -81,22 +81,22 @@ end
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Step #3 - Alignment Shift Count
-logic [5:0] a_cnt;
-assign a_cnt = ((pe != 16) & pe[5] & ze[4] & (pe[4:0] < ze)) ? ((pe - ze) == 5'b10000) ? (pe - ze) : (pe - ze - 32) : (pe - ze); //  - 6'b001111 - 6'b001111;   // maximum is 32
+logic [5:0] a_cnt; // changed from 5'b10000 to 6'b110000
+assign a_cnt = ((pe != 16) & pe[5] & ze[4] & (pe[4:0] < ze)) ? ((pe - ze) == 6'b110000) ? (pe - ze) : (pe - ze - 32) : (pe - ze); //  - 6'b001111 - 6'b001111;   // maximum is 32
 
 logic [5:0] a_cnt_pos;
 assign a_cnt_pos = { ~a_cnt[5], ~a_cnt[4], ~a_cnt[3], ~a_cnt[2], ~a_cnt[1], ~a_cnt[0]} + 1; // 2's complement of a_cnt
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Step #4 - Alignment Mantissa
-logic [146:0] zm_bf_shift;
-logic [146:0] am;
+logic [136:0] zm_bf_shift;
+logic [136:0] am;
 assign zm_bf_shift = { 63'h0, 1'b1, zm, 63'h0 };
 assign am = (a_cnt[5]) ? zm_bf_shift << a_cnt_pos : (zm_bf_shift >> a_cnt);    // left shift
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Step #5 - Sum Mantissa
-logic [146:0] sm;
+logic [136:0] sm;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Step #6 - Normalization Shift
@@ -107,17 +107,17 @@ always_comb begin // logic based off FMA Detailed Algorithm
 end
 
 logic [7:0] m_cnt_2;
-logic [146:0] check;
+logic [136:0] check;
 always_comb begin
     m_cnt_2 = -40;
     check = (pm > am) ? (pm - am) : (am - pm);
     if (zs & ~ps & ~z_zero) begin // if z is negative
         // if a_cnt is too big, then we can see am - pe to check sm, before computing its actual location. If a_cnt is big enough, we don't need to worry about the "middle zone"
         if (a_cnt[5] & (a_cnt != -6'd2) & (a_cnt != -6'd1)) begin // a_cnt = -3 to all other negatives
-            if (check[73 + a_cnt_pos + 1])      m_cnt_2 = { {8{1'b1}}, a_cnt - 1'b1 };
-            else if (check[73 + a_cnt_pos + 0]) m_cnt_2 = { {8{1'b1}}, a_cnt };
-            else if (check[73 + a_cnt_pos - 1]) m_cnt_2 = { {8{1'b1}}, (a_cnt + 1'b1) };
-            else if (check[73 + a_cnt_pos - 2]) m_cnt_2 = { {8{1'b1}}, (a_cnt + 2'b10) };
+            if (check[73 + a_cnt_pos + 1'b1])      m_cnt_2 = { {2{1'b1}}, a_cnt - 1'b1 };
+            else if (check[73 + a_cnt_pos + 0]) m_cnt_2 = { {2{1'b1}}, a_cnt };
+            else if (check[73 + a_cnt_pos - 1'b1]) m_cnt_2 = { {2{1'b1}}, (a_cnt + 1'b1) };
+            else if (check[73 + a_cnt_pos - 6'b000010]) m_cnt_2 = { {2{1'b1}}, (a_cnt + 6'b000010) };
 
             sm = check;
         end else begin  // a_cnt is -2 or above, we need to check that 74 or 73 doesn't have any bits
@@ -145,8 +145,8 @@ always_comb begin
     end else if (~zs & ps & ~z_zero) begin
         
         if (a_cnt[5] & (a_cnt != -6'd2) & (a_cnt != -6'd1)) begin // a_cnt = -3 to all other negatives
-            if (check[73 + a_cnt_pos])          m_cnt_2 = { {8{1'b1}}, a_cnt};
-            else if (check[73 + a_cnt_pos - 1]) m_cnt_2 = { {8{1'b1}}, (a_cnt+1'b1)};
+            if (check[73 + a_cnt_pos])          m_cnt_2 = { {2{1'b1}}, a_cnt};
+            else if (check[73 + a_cnt_pos - 1]) m_cnt_2 = { {2{1'b1}}, (a_cnt+1'b1)};
 
             sm = check;
         end else begin  // a_cnt is -2 or above, we need to check that 74 or 73 doesn't have any bits
@@ -180,9 +180,9 @@ always_comb begin
     end else begin          // if z is positive
         sm = (z_zero) ? pm : (am + pm); //(zs) ? (pm - am) : (am + pm);
         if (a_cnt[5]) begin
-            if      (sm[73 + a_cnt_pos + 1])    m_cnt_2 = (a_cnt == 6'b100000) ?  8'b1111_1111 : { {8{1'b1}}, (a_cnt - 1'b1) };
-            else if (sm[73 + a_cnt_pos])        m_cnt_2 = { {8{1'b1}}, a_cnt };
-            else if (sm[73 + a_cnt_pos - 1])    m_cnt_2 = (a_cnt == -6'd1) ? 0 : {  {8{1'b1}}, (a_cnt + 1'b1) };
+            if      (sm[73 + a_cnt_pos + 1])    m_cnt_2 = (a_cnt == 6'b100000) ?  8'b1111_1111 : { {2{1'b1}}, (a_cnt - 1'b1) };
+            else if (sm[73 + a_cnt_pos])        m_cnt_2 = { {2{1'b1}}, a_cnt };
+            else if (sm[73 + a_cnt_pos - 1])    m_cnt_2 = (a_cnt == -6'd1) ? 0 : {  {2{1'b1}}, (a_cnt + 1'b1) };
         end else begin
             if      (sm[75])                    m_cnt_2 = -2;
             else if (sm[74])                    m_cnt_2 = -1;
@@ -197,17 +197,17 @@ always_comb begin
     else        m_shift = m_cnt_2;
 end
 
-logic [8:0] error;
-assign error = ((pe != 16) & pe[5] & ze[4] & (pe[4:0] < ze)) ? pe - ze - 32 : pe - ze;
+// logic [8:0] error;
+// assign error = ((pe != 16) & pe[5] & ze[4] & (pe[4:0] < ze)) ? pe - ze - 32 : pe - ze;
 
-logic error_2;
-assign error_2 = ((xs & ~ys) | (~xs & ys));
+// logic error_2;
+// assign error_2 = ((xs & ~ys) | (~xs & ys));
 //  ((pe != 16) & pe[5] & ze[4] & (pe[4:0] < ze)) ? ((pe - ze) == 5'b10000) ? (pe - ze) : (pe - ze - 32) : (pe - ze)
 // assign error_2 =   ((pe != { ze[4], ze }) & (pe-ze == 0)) ? pe - { ze[4], ze } : ((pe != 16) & pe[5] & ze[4] & (pe[4:0] < ze)) ? ((pe - ze) == 5'b10000) ? (pe - ze) : (pe - ze - 32) : (pe - ze); // 2's complement of ze
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Step #7 - Normalization Mantissa and Exponent
-logic [146:0] mm;
+logic [136:0] mm;
 logic [9:0] mm_part;
 logic [4:0] me;
 logic [7:0] index;
@@ -215,8 +215,15 @@ logic [7:0] index;
 logic [7:0] pos_m_shift;
 assign pos_m_shift = ({~m_shift[7], ~m_shift[6], ~m_shift[5], ~m_shift[4], ~m_shift[3], ~m_shift[2], ~m_shift[1], ~m_shift[0]} + 1); // 2's complement of m_cnt
 
-assign mm_part = ((pe != 16) & pe[5] & ze[4] & (pe[4:0] < ze)) ? mm[104:93] : mm[72:63];
-assign me = (m_shift == 0) ? pe : (m_shift[7]) ? (pe + pos_m_shift) : (pe - m_shift); // 2's complement of m_cnt : (pe - m_shift);
+assign mm_part = ((pe != 16) & pe[5] & ze[4] & (pe[4:0] < ze)) ? mm[104:95] : mm[72:63];
+
+logic [7:0] sum_pe;
+assign sum_pe = { {2{pe[5]}}, pe} + pos_m_shift;
+
+logic [7:0] dif_pe;
+assign dif_pe = { {2{pe[5]}}, pe} - m_shift;
+
+assign me = ( m_shift == 8'b0) ? (pe[4:0]) : (m_shift[7]) ?  sum_pe[4:0] : dif_pe[4:0]; // 2's complement of m_cnt : (pe - m_shift);
 
 assign mm = m_shift[7] ? (sm >>> pos_m_shift) : sm <<< m_shift; // (m_shift[7]) ? sm >> m_shift : sm >> m_shift;
 
